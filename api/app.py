62aa223286411febd2a316cb9ce97baf42be3589
Murad Kamali <murad.kamali@clock.co.uk>
2023-01-12 20:09:41 +0000

    API now formats the data correctly to pass into the AI model

--- api/app.py
+++ api/app.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 
 from audiModelFields import audiKeyFields
-print(audiKeyFields)
 # creating the flask app
 app = Flask(__name__)
 # creating an API object
@@ -14,7 +13,7 @@ api = Api(app)
 # importing models  
 
 modelDict = {
-    'Audi': pickle.load(open('audiModel.pickle', 'rb')),
+    'Audi': pickle.load(open('audimodel.pickle', 'rb')),
     # 'Volkswagen': pickle.load(open('volkswagenModel.pickle', 'rb'))
 }
 car_post_args_parser = reqparse.RequestParser()
@@ -27,27 +26,33 @@ car_post_args_parser.add_argument('fuelType', type=str, help='Requires fuelType'
 car_post_args_parser.add_argument('engineSize', type=float, help='Requires engineSize')
 
 def formatData(args):
+    manufacturer = args['manufacturer']
+    del args['manufacturer']
+    model = modelDict[manufacturer]
     headersToFormat = ['model', 'transmission', 'fuelType']
     for header in headersToFormat:
         args[header] = 'dmy_' + args[header]
-    print(5444, args)
-    # tempData = []
-    # for field in audiKeyFields:
-    #     if args[field]:
-    #         tempData.append(args[field])
-    #     else:
-    #         tempData.append(0)
-
-    # for i in range (audiKeyFields.length):
-    #     if audiKeyFields[i] == 
-
-    
-
-
-    data = np.array(tempData)
-
-    manufacturer = args['manufacturer']
-    return manufacturer, data
+   
+    # Potential Solution:
+    # Convert the args (input data) into an array using only the args values
+    input_data = list(args.values())
+    # Create empty array (finalInputData) for input data to pass into the model
+    finalInputData = []
+    # Loop through the KeyFieldsArray (e.g., audiKeyFields)
+        # Each iteration check the keyfields array value and see if it exists inside the args array
+        # If it does exist, append 2 to finalInputData array
+        # Else, append 0 value to finalInputData array
+    # First for loop is for appending the non-dummy variables to the array
+    for i in range(0, 3):
+        finalInputData.append(input_data[i])
+    for i in range (3, len(audiKeyFields)):
+        if audiKeyFields[i] in input_data:
+            finalInputData.append(1)
+        else:
+            finalInputData.append(0)
+    data = np.array(finalInputData)
+    data = [data]
+    return model, data
 
 class PredictPrice(Resource):
   
@@ -57,14 +62,10 @@ class PredictPrice(Resource):
   
     # Corresponds to POST request
     def post(self):
-        args = car_post_args_parser.parse_args()
-
-        manufacturer, data = formatData(args)
-        model = modelDict[manufacturer]
- 
+        args = request.json
+        model, data = formatData(args)
         practice = model.predict(data)
         practiceList = practice.tolist()
-        print(practiceList)
         return jsonify({'data': practiceList})
   
   
